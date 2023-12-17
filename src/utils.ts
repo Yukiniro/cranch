a55a60af36146ad457ed4e633fde4cec9d5e1b87
Yukiniro <yukiniro@hotmail.com>
2023-12-17 17:27:14 +0800

    fix: fix branchname start with -

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,16 @@
 import { nanoid } from "nanoid";
 
+function generateRandomLetter(): string {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  return alphabet[Math.floor(Math.random() * alphabet.length)];
+}
+
 function generateGitBranchName(): string {
   let branchName = nanoid(8);
-  // eslint-disable-next-line no-useless-escape
-  branchName = branchName.replace(/[^\d/_a-z\-]/g, "-"); // 替换所有不符合规则的字符
-  branchName = branchName.replace(/^\.|\/$|\.\.|\/\/+/g, "-"); // 确保字符串不以斜杠或点开始或结束，不包含连续的斜杠或点
+  // 替换所有不符合规则的字符为随机字母
+  branchName = branchName.replace(/[^\d/_a-z]/g, generateRandomLetter);
+  // 确保字符串不以斜杠或点开始或结束，不包含连续的斜杠或点
+  branchName = branchName.replace(/^\.|\/$|\.\.|\/\/+/g, generateRandomLetter);
   return branchName;
 }
 
