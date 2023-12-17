ad59a4657e4a44b46671e708690440f0c6ff2b5c
Yukiniro <yukiniro@hotmail.com>
2023-12-17 17:11:36 +0800

    feat: branchName default value is 8

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { nanoid } from "nanoid";
 
 function generateGitBranchName(): string {
-  let branchName = nanoid();
+  let branchName = nanoid(8);
   // eslint-disable-next-line no-useless-escape
   branchName = branchName.replace(/[^\d/_a-z\-]/g, "-"); // 替换所有不符合规则的字符
   branchName = branchName.replace(/^\.|\/$|\.\.|\/\/+/g, "-"); // 确保字符串不以斜杠或点开始或结束，不包含连续的斜杠或点
